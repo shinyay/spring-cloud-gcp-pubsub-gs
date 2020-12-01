fa602b1dd619dc1aed25cbb6df43234e6555648f
shinyay <shinya.com@gmail.com>
2020-12-01 23:53:42 +0900

    Add publish message function skelton

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -1,8 +1,12 @@
 package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController(val pubSubTemplate: PubSubTemplate) {
+
+    @PostMapping("/message")
+    fun publish() {}
 }
