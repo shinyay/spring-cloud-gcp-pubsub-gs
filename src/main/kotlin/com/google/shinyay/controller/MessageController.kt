6c3e38f251c4115c8db3bc4b04ce7ca6e13227c1
shinyay <shinya.com@gmail.com>
2020-12-02 13:18:27 +0900

    Add return to view page

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -4,13 +4,15 @@ import com.google.cloud.spring.pubsub.core.PubSubTemplate
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 class MessageController(val pubSubTemplate: PubSubTemplate) {
 
     @PostMapping("/message")
     fun publish(@RequestParam topicName: String,
-                @RequestParam message: String) {
+                @RequestParam message: String): RedirectView {
         pubSubTemplate.publish(topicName, message)
+        return RedirectView("/")
     }
 }
