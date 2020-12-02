cdfb9a38a1f502ce6b6e3a7489f8055fb38b7a07
shinyay <shinya.com@gmail.com>
2020-12-02 23:13:48 +0900

    Add pull and ack message from topic

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
 import com.google.cloud.spring.pubsub.support.AcknowledgeablePubsubMessage
+import com.google.shinyay.logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,12 +13,21 @@ class MessageController(val pubSubTemplate: PubSubTemplate) {
 
     @PostMapping("/message")
     fun publishMessage(@RequestParam topicName: String,
-                @RequestParam message: String) {
+                       @RequestParam message: String) {
         pubSubTemplate.publish(topicName, message)
     }
 
     @GetMapping("/message")
     fun viewMessages(@RequestParam subscriptionName: String): String {
-        return pubSubTemplate.pull(subscriptionName, 10, true).toString()
+        val messages = pubSubTemplate.pull(subscriptionName, 10, true)
+        logger.info(messages.toString())
+        messages.forEach { msg ->
+            logger.info("ACK: ${msg.ackId}")
+            msg.ack()
+        }
+        return """
+            Pulled Message size: ${messages.size}
+            ${messages.toString()}
+        """.trimIndent()
     }
 }
