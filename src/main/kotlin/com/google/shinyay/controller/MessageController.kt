57d726a860a594acfba7c08133e41ffd91693d88
shinyay <shinya.com@gmail.com>
2020-12-02 13:16:53 +0900

    Add message publish function

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -2,11 +2,15 @@ package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController(val pubSubTemplate: PubSubTemplate) {
 
     @PostMapping("/message")
-    fun publish() {}
+    fun publish(@RequestParam topicName: String,
+                @RequestParam message: String) {
+        pubSubTemplate.publish(topicName, message)
+    }
 }
