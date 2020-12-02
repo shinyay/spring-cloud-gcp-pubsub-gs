cf2faab634e6fb176f6497cd7896120ee04ff88e
shinyay <shinya.com@gmail.com>
2020-12-02 18:05:47 +0900

    Remove redirect to web page

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -1,18 +1,20 @@
 package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.servlet.view.RedirectView
 
 @RestController
 class MessageController(val pubSubTemplate: PubSubTemplate) {
 
     @PostMapping("/message")
-    fun publish(@RequestParam topicName: String,
-                @RequestParam message: String): RedirectView {
+    fun publishMessage(@RequestParam topicName: String,
+                @RequestParam message: String) {
         pubSubTemplate.publish(topicName, message)
-        return RedirectView("/")
     }
+
+    @GetMapping("/message")
+    fun viewMessages(@RequestParam)
 }
