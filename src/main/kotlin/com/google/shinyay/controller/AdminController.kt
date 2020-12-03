e698a90353d4858446f311df9fcd3d70d1d6ea69
shinyay <shinya.com@gmail.com>
2020-12-03 23:17:19 +0900

    Add delete topic

--- src/main/kotlin/com/google/shinyay/controller/AdminController.kt
+++ src/main/kotlin/com/google/shinyay/controller/AdminController.kt
@@ -2,6 +2,7 @@ package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.PubSubAdmin
 import com.google.shinyay.logger
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -20,5 +21,10 @@ class AdminController(val pubSubAdmin: PubSubAdmin) {
                            @RequestParam topicName: String) {
         val subscription = pubSubAdmin.createSubscription(subscriptionName, topicName)
         logger.info("Created: ${subscription.name}")
+    }
+
+    @DeleteMapping("/topic")
+    fun deleteTopic(@RequestParam topicName: String) {
+        pubSubAdmin.deleteTopic(topicName)
     }
 }
