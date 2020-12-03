f65fb5751c05e3a0faaa1638d8fa9363503dea63
shinyay <shinya.com@gmail.com>
2020-12-03 22:57:16 +0900

    Add create Topic

--- src/main/kotlin/com/google/shinyay/controller/AdminController.kt
+++ src/main/kotlin/com/google/shinyay/controller/AdminController.kt
@@ -1,8 +1,15 @@
 package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.PubSubAdmin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AdminController(val pubSubAdmin: PubSubAdmin) {
+
+    @PostMapping("/topic")
+    fun createTopic(@RequestParam topicName: String) {
+        pubSubAdmin.createTopic(topicName)
+    }
 }
