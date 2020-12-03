e5250f671a8804ad29802e8edf05a612a22925a0
shinyay <shinya.com@gmail.com>
2020-12-03 10:23:08 +0900

    Add Subscribe topic to display message as system out

--- src/main/kotlin/com/google/shinyay/controller/MessageController.kt
+++ src/main/kotlin/com/google/shinyay/controller/MessageController.kt
@@ -1,12 +1,15 @@
 package com.google.shinyay.controller
 
+import com.google.cloud.pubsub.v1.Subscriber
 import com.google.cloud.spring.pubsub.core.PubSubTemplate
-import com.google.cloud.spring.pubsub.support.AcknowledgeablePubsubMessage
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage
 import com.google.shinyay.logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.view.RedirectView
+
 
 @RestController
 class MessageController(val pubSubTemplate: PubSubTemplate) {
@@ -30,5 +33,14 @@ class MessageController(val pubSubTemplate: PubSubTemplate) {
             Pulled Message size: ${messages.size}
             ${messages.toString()}
         """.trimIndent()
+    }
+
+    @GetMapping("/subscription")
+    fun subscribe(@RequestParam subscriptionName: String) {
+        val subscriber: Subscriber = pubSubTemplate.subscribe(subscriptionName) { message: BasicAcknowledgeablePubsubMessage ->
+            logger.info("Message received from " + subscriptionName + " subscription: "
+                    + message.pubsubMessage.data.toStringUtf8())
+            message.ack()
+        }
     }
 }
