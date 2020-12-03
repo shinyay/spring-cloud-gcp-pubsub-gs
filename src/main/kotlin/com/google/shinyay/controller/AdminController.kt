a6c3e4d3edb82687e1c92ffd301312312a61b195
shinyay <shinya.com@gmail.com>
2020-12-03 23:01:59 +0900

    Add info message

--- src/main/kotlin/com/google/shinyay/controller/AdminController.kt
+++ src/main/kotlin/com/google/shinyay/controller/AdminController.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay.controller
 
 import com.google.cloud.spring.pubsub.PubSubAdmin
+import com.google.shinyay.logger
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -11,5 +12,6 @@ class AdminController(val pubSubAdmin: PubSubAdmin) {
     @PostMapping("/topic")
     fun createTopic(@RequestParam topicName: String) {
         pubSubAdmin.createTopic(topicName)
+        logger.info("Create Topic: $topicName")
     }
 }
