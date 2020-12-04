3afbb140b23cdb94a99d65f16091ef3354cd8558
shinyay <shinya.com@gmail.com>
2020-12-04 10:19:40 +0900

    Add topic create for test functions

--- src/test/kotlin/com/google/shinyay/SpringCloudGcpPubsubGsApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/SpringCloudGcpPubsubGsApplicationTests.kt
@@ -1,10 +1,31 @@
 package com.google.shinyay
 
+import com.google.cloud.ServiceOptions
+import com.google.cloud.pubsub.v1.SubscriptionAdminClient
+import com.google.cloud.pubsub.v1.TopicAdminClient
+import com.google.pubsub.v1.ProjectName
+import com.google.pubsub.v1.PushConfig
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.event.annotation.BeforeTestClass
 
 @SpringBootTest
 class SpringCloudGcpPubsubGsApplicationTests {
+
+	lateinit var projectName:String
+	lateinit var topicAdminClient: TopicAdminClient
+	lateinit var subscriptionAdminClient: SubscriptionAdminClient
+	val topicName = "test-topic"
+
+	@BeforeTestClass
+	fun prepare() {
+		projectName = ProjectName.of(ServiceOptions.getDefaultProjectId()).project
+		topicAdminClient = TopicAdminClient.create()
+		subscriptionAdminClient = SubscriptionAdminClient.create()
+
+		topicAdminClient.createTopic(topicName)
+		subscriptionAdminClient.createSubscription(projectName, topicName, PushConfig.getDefaultInstance(), 10)
+	}
 
 	@Test
 	fun contextLoads() {
