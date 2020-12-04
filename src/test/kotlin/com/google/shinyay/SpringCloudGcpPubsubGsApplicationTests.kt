32964810b6fc88b1a0fc2acd9b4d67399c6e812b
shinyay <shinya.com@gmail.com>
2020-12-04 10:44:09 +0900

    Modify test case initialization to use @BeforeAll

--- src/test/kotlin/com/google/shinyay/SpringCloudGcpPubsubGsApplicationTests.kt
+++ src/test/kotlin/com/google/shinyay/SpringCloudGcpPubsubGsApplicationTests.kt
@@ -4,11 +4,14 @@ import com.google.cloud.ServiceOptions
 import com.google.cloud.pubsub.v1.SubscriptionAdminClient
 import com.google.cloud.pubsub.v1.TopicAdminClient
 import com.google.pubsub.v1.ProjectName
-import com.google.pubsub.v1.PushConfig
+import com.google.pubsub.v1.ProjectTopicName
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.event.annotation.BeforeTestClass
+import org.springframework.test.context.event.annotation.AfterTestClass
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 class SpringCloudGcpPubsubGsApplicationTests {
 
@@ -17,16 +20,37 @@ class SpringCloudGcpPubsubGsApplicationTests {
 	lateinit var subscriptionAdminClient: SubscriptionAdminClient
 	val topicName = "test-topic"
 
-	@BeforeTestClass
-	fun prepare() {
+	@BeforeAll
+	fun initAll() {
 		projectName = ProjectName.of(ServiceOptions.getDefaultProjectId()).project
 		topicAdminClient = TopicAdminClient.create()
 		subscriptionAdminClient = SubscriptionAdminClient.create()
 
-		topicAdminClient.createTopic(topicName)
-		subscriptionAdminClient.createSubscription(projectName, topicName, PushConfig.getDefaultInstance(), 10)
+		topicAdminClient.createTopic(ProjectTopicName.of(projectName, topicName))
+//		subscriptionAdminClient.createSubscription(projectName, topicName, PushConfig.getDefaultInstance(), 10)
 	}
 
+	@AfterTestClass
+	fun cleanupPubsubClients() {
+
+	}
+
+//	private fun deleteSubscriptions(vararg testSubscriptions: String) {
+//		for (testSubscription in testSubscriptions) {
+//			val testSubscriptionName = ProjectSubscriptionName.format(
+//					projectName, testSubscription)
+//			val projectSubscriptions: List<String>? = getSubscriptionNamesFromProject()
+//			if (projectSubscriptions.contains(testSubscriptionName)) {
+//				subscriptionAdminClient.deleteSubscription(testSubscriptionName)
+//			}
+//		}
+//	}
+//	private fun getSubscriptionNamesFromProject(): List<String>? {
+//		val response = subscriptionAdminClient.listSubscriptions("projects/$projectName")
+//		return StreamSupport.stream(response.iterateAll().spliterator(), false)
+//				.map<Any>(Subscription::getName)
+//				.collect(Collectors.toList<Any>())
+//	}
 	@Test
 	fun contextLoads() {
 	}
